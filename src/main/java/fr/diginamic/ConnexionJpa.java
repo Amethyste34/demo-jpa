5e59343a03825ab970639d68ad67e862f3dac998
Amethyste34 <laurence.amethyste@gmail.com>
2026-07-15 11:51:21 +0200

    Première connexion à la BDD

---
+++ src/main/java/fr/diginamic/ConnexionJpa.java
@@ -0,0 +1,18 @@
+package fr.diginamic;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class ConnexionJpa {
+
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu_essai");
+        EntityManager em = entityManagerFactory.createEntityManager();
+
+        System.out.println("Connexion établie avec succès !");
+
+        em.close();
+        entityManagerFactory.close();
+    }
+}
