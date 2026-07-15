22937d4944ffcf2ad2531a7a290c44db4a05f4e7
Amethyste34 <laurence.amethyste@gmail.com>
2026-07-15 14:53:43 +0200

    création nouvelle région depuis java

--- src/main/java/fr/diginamic/ConnexionJpa.java
+++ src/main/java/fr/diginamic/ConnexionJpa.java
@@ -2,6 +2,7 @@ package fr.diginamic;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class ConnexionJpa {
@@ -16,6 +17,16 @@ public class ConnexionJpa {
         // Affichage du nom
         System.out.println("Nom de la région : " + region.getNom());
 
+        // Je veux insérer une nouvelle région
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+
+        Region rhoneAlpes = new Region();
+        rhoneAlpes.setNom("Rhône-Alpes");
+
+        em.persist(rhoneAlpes);
+
+        transaction.commit();
 
         em.close();
         entityManagerFactory.close();
